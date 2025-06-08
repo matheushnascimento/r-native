d3faf82d559d1e0b370a6c67ce7b50dff57fa3c7
matheushnascimento <matheus.mhddn@gmail.com>
2025-06-08 15:11:12 -0300

    build: install and config `nativewind` package

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["app/**/*.*"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
