78051581b7a1dbafcaef7cc8f105418eeef22e9a
matheushnascimento <matheus.mhddn@gmail.com>
2025-06-09 21:29:27 -0300

    refactor: update index.jsx file style

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,32 @@
-import "@/global.css";
-import { Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Link } from "expo-router";
 
 import Logo from "../assets/favicon.png";
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View style={styles.container}>
       <Image source={Logo} className="mx-5" />
-      <Text className="font-bold text-[18px] text-[#800080]">The Number 1</Text>
+      <Text className="font-bold text-[18px]">The Number 1</Text>
       <Text className="mt-[10px] mb-[30px]">Reading List App</Text>
-      <View className="bg-[#eee] p-5 rounded-md shadow-[4px_4px_rgba(0,0,0,0.1)]">
+      <View className="bg-[#eee] p-5  mb-7 rounded-md shadow-[4px_4px_rgba(0,0,0,0.1)]">
         <Text>Hello, this is a card.</Text>
         <Text className="text-red-500">Testing nativewind.</Text>
       </View>
-      <Link href="/about">About Page</Link>
+      <Link href="/about" className="mx-2 border-b-[1px]">
+        About Page
+      </Link>
+      <Link href="/contact" className="mx-2 border-b-[1px]">
+        Contact Page
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
