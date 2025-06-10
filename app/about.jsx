4efc0b324dda735f46087f19ceaf75c7ea233956
matheushnascimento <matheus.mhddn@gmail.com>
2025-06-09 21:29:10 -0300

    feat: update `about.jsx` file

--- app/about.jsx
+++ app/about.jsx
@@ -1,9 +1,25 @@
-import { View, Text } from "react-native";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { Link } from "expo-router";
+import { Colors } from "@/constants/Colors";
 
 export default function About() {
+  const colorScheme = useColorScheme();
+  console.log(colorScheme);
+  const theme = Colors[colorScheme] ?? Colors.light;
   return (
-    <View>
-      <Text>About Page</Text>
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <Text className="font-bold text-[18px]">About Page</Text>
+      <Link href="/" className="mx-2 border-b-[1px]">
+        Back Home
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
