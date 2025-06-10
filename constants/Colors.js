7865c26c8e7b57e2efb582f1f04a18908454e6cb
matheushnascimento <matheus.mhddn@gmail.com>
2025-06-09 21:28:37 -0300

    feat: create `Colors.js` file

---
+++ constants/Colors.js
@@ -0,0 +1,23 @@
+export const Colors = {
+  primary: "#6849a7",
+  warning: "#cc475a",
+
+  dark: {
+    text: "#d4d4d4",
+    title: "#fff",
+    background: "#252231",
+    navBackground: "#2e1e2b",
+    iconC010r: "#9591a5",
+    uiBackground: "#2f2b3d",
+  },
+
+  light: {
+    text: "#625f72",
+    title: "#2e1e2b",
+    background: "#eedfe8",
+    navBackground: "#e8e7ef",
+    iconC010r: "#686477",
+    iconC010rFocused: "#2e1e2b",
+    uiBackground: "#d6d5e1",
+  },
+};
