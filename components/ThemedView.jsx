9bc85b64f451baae5cb020f9da45ee34825c8496
matheushnascimento <matheus.mhddn@gmail.com>
2025-08-13 16:58:15 -0300

    feat: adds safeArea settings to Themed View

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,10 +1,31 @@
 import { Colors } from "@/constants/Colors";
 import { useColorScheme, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function ThemedView({ style, ...props }) {
+export default function ThemedView({ style, safe = true, ...props }) {
   const colorScheme = useColorScheme();
   const themes = Colors[colorScheme] || Colors.light;
+  if (!safe)
+    return (
+      <View
+        style={[{ backgroundColor: themes.background }, style]}
+        {...props}
+      />
+    );
+
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={[{ backgroundColor: themes.background }, style]} {...props} />
+    <View
+      style={[
+        {
+          backgroundColor: themes.background,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+        },
+        style,
+      ]}
+      {...props}
+    />
   );
 }
